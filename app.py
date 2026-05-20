d2ff9223f0c6940ff26297a6bacbd9993e6e24a1
Jordan <jordanascencio16@gmail.com>
2026-05-20 00:35:11 -0400

    mi app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/entrar', methods=['POST'])
+def entrar():
+    fecha = request.form['fecha']
+
+    if(fecha == '15/3/2024'):
+        return render_template('carta.html')
+    else:
+        return render_template('error.html')
+
+app.run(host='0.0.0.0', debug=True)
